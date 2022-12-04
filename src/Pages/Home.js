0e632982597e3c511b00e446169fd457e59a5760
Mossahid123 <mossahidhossain309@gmail.comm>
2022-12-04 22:49:18 +0600

    productcard has created

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
+    const [product , setProduct] =useState([]);
+    useEffect(() =>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProduct(data))
+    },[])
+    console.log(product);
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+            <h1>This is Home page</h1>
         </div>
     );
 };
