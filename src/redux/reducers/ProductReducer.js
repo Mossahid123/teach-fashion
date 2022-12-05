7f6c60a2f660eb1e3804e6d1135e757bc2b744a6
Mossahid123 <mossahidhossain309@gmail.comm>
2022-12-05 07:08:57 +0600

    created cart logic functon

--- src/redux/reducers/ProductReducer.js
+++ src/redux/reducers/ProductReducer.js
@@ -1,8 +1,23 @@
+import { ADD_TO_CART } from "../actionTypes/actionTypes";
+
+
 const initialState = {
-    test: "TEST"
+    Cart:[]
 };
 
 const ProductReducer = (state = initialState , action ) => {
+switch (action.type) {
+    case ADD_TO_CART:
+        
+    return {
+        ...state ,
+        cart:[...state.cart , action.payload]
+    }
+
+    default:
+        return state;
+}
+
     return state;
 }
 export default ProductReducer;
